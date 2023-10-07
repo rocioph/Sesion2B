183e3b5e25f4d2e2259d903e38744b46eabf394c
rocioph <rocioph70@gmail.com>
2023-10-07 13:37:21 +0200

    commit test

--- src/emp/empleado.java
+++ src/emp/empleado.java
@@ -1,38 +1,49 @@
 package emp;
 
 public class empleado {
-	 public enum TipoEmpleado{Vendedor, Encargado}
-	 float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtra) 
+	 public enum TipoEmpleado{Vendedor, Encargado, Otro}
+	 public float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtra) 
 	 {
 		 float salariobase=0, extras=0, primas=0;
-		 if(tipo==TipoEmpleado.Encargado)
+		 //determinacion salario base
+		 if(tipo==TipoEmpleado.Encargado || tipo==TipoEmpleado.Vendedor)
 		 {
-			 salariobase=2500;
+			 if(tipo==TipoEmpleado.Encargado)
+				 salariobase=2500;
+			 else if(tipo==TipoEmpleado.Vendedor) 
+				 salariobase=2000;
+			 if(ventasMes>=1500)
+				 primas=200;
+			 
+			 else if(ventasMes>=1000)
+				 primas=100;
+			 if(horasExtra<0)
+				 extras=0;
+			 else
+				 extras=horasExtra*30;
+			 return salariobase+primas+extras;
 		 }
-		 else if(tipo==TipoEmpleado.Vendedor) 
-		 {
-			 salariobase=2000;
-		 }
-		 if(ventasMes>=1500)
-			 primas=200;
-		 else if(ventasMes>=1000)
-			 primas=100;
-		 extras=horasExtra*30;
-		 return salariobase+primas+extras;
+		 return 0;
 	 }
-	float calculoNominaNeta(float nominaBruta)
+	public float calculoNominaNeta(float nominaBruta)
 	{
 		float retencion=0;
-		if(nominaBruta>2500)
+		if(nominaBruta<0)
 		{
-			retencion=0.18f;
+			return 0;
 		}
-		else if(nominaBruta>2000)
+		else
 		{
-			retencion=0.15f;
+			if(nominaBruta>2500)
+			{
+				retencion=0.18f;
+			}
+			else if(nominaBruta>2000)
+			{
+				retencion=0.15f;
+			}
+			return nominaBruta*(1-retencion);
 		}
-		return retencion;
-		
 	}
 	
 }
--- src/test/TipoEmpleadoTest.java
+++ src/test/TipoEmpleadoTest.java
@@ -1,13 +1,11 @@
 package test;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import emp.empleado;
 class TipoEmpleadoTest {
 
 	@BeforeAll
@@ -27,8 +25,94 @@ class TipoEmpleadoTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCalculoNominaBrutaEncargado() {
+		empleado emp = new empleado();
+		float nominaBruta = emp.calculoNominaBruta(empleado.TipoEmpleado.Encargado, 1500, 0);
+		assertEquals(2700, nominaBruta);
+		
+	}
+	
+	@Test
+	void testCalculoNominaBrutaVendedor() {
+		empleado emp = new empleado();
+		float nominaBruta = emp.calculoNominaBruta(empleado.TipoEmpleado.Vendedor, 1500, 0);
+		assertEquals(2200, nominaBruta);
+	}
+	
+	@Test
+	void testCalculoNominaBrutaOtro() {
+		empleado emp = new empleado();
+		float nominaBruta = emp.calculoNominaBruta(empleado.TipoEmpleado.Otro, 1500, 0);
+		assertEquals(0, nominaBruta);
+	}
+	
+	@Test
+	void testCalculoNominaBrutaVentasMesNegativa() {
+		empleado emp = new empleado();
+		float nominaBruta = emp.calculoNominaBruta(empleado.TipoEmpleado.Vendedor, -900, 0);
+		assertEquals(2000, nominaBruta);
+	}
+	
+	@Test
+	void CalculoNominaBrutaVentasMesMenor1000() {
+		empleado emp = new empleado();
+		float nominaBruta = emp.calculoNominaBruta(empleado.TipoEmpleado.Vendedor, 900, 0);
+		assertEquals(2000, nominaBruta);
 	}
 
+	@Test
+	void CalculoNominaBrutaVentasMesEntre1000y1500() {
+		empleado emp = new empleado();
+		float nominaBruta = emp.calculoNominaBruta(empleado.TipoEmpleado.Vendedor, 1100, 0);
+		assertEquals(2100, nominaBruta);
+	}
+
+	@Test
+	void CalculoNominaBrutaVentasMesMayor1500() {
+		empleado emp = new empleado();
+		float nominaBruta = emp.calculoNominaBruta(empleado.TipoEmpleado.Vendedor, 1600, 0);
+		assertEquals(2200, nominaBruta);
+	}
+
+	@Test
+	void CalculoNominaBrutaVentasHorasExtraPositivas() {
+		empleado emp = new empleado();
+		float nominaBruta = emp.calculoNominaBruta(empleado.TipoEmpleado.Vendedor, 900, 2);
+		assertEquals(2060, nominaBruta);
+	}
+
+	@Test
+	void CalculoNominaBrutaVentasHorasExtraNegativas() {
+		empleado emp = new empleado();
+		float nominaBruta = emp.calculoNominaBruta(empleado.TipoEmpleado.Vendedor, 900, -2);
+		assertEquals(2000, nominaBruta);
+	}
+	
+	@Test
+	void testCalculoNominaNetaNegativa() {
+		empleado emp = new empleado();
+		float nominaNeta = emp.calculoNominaNeta(-1000);
+		assertEquals(0, nominaNeta);
+	}
+	
+	@Test
+	void testCalculoNominaNetaMenos2100() {
+		empleado emp = new empleado();
+		float nominaNeta = emp.calculoNominaNeta(2000);
+		assertEquals(2000, nominaNeta);
+	}
+	
+	@Test
+	void testCalculoNominaNetaEntre2100y2500() {
+		empleado emp = new empleado();
+		float nominaNeta = emp.calculoNominaNeta(2200);
+		assertEquals(1870, nominaNeta);
+	}
+	
+	@Test
+	void testCalculoNominaNetaMayor2500() {
+		empleado emp = new empleado();
+		float nominaNeta = emp.calculoNominaNeta(2600);
+		assertEquals(2132, nominaNeta);
+	}
 }
